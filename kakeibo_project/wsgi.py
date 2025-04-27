b87f092450472728e570f30f53e56565acdf8964
yuya <2333340431c@kindai.ac.jp>
2025-04-27 18:08:47 +0900

    Temporarily add auto-migrate in wsgi.py

--- kakeibo_project/wsgi.py
+++ kakeibo_project/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kakeibo_project.settings')
+
+django.setup()
+call_command('migrate')  # ← migrateを自動実行！
+
 
 application = get_wsgi_application()
