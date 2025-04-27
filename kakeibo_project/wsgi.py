d2b3ecf722325ad75c83ccd67c11a5247c6fe7a5
yuya <2333340431c@kindai.ac.jp>
2025-04-27 18:31:06 +0900

    Finalize wsgi.py for production

--- kakeibo_project/wsgi.py
+++ kakeibo_project/wsgi.py
@@ -4,19 +4,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kakeibo_project.settings')
 
-# ===== ここから追加 =====
-import django
-django.setup()
-from django.core.management import call_command
-call_command('migrate')
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-# ここでユーザーがまだいなければ管理者ユーザー作成
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
-# ===== ここまで追加 =====
-
 application = get_wsgi_application()
