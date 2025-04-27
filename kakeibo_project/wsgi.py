db2d6b3a2261ee39c9549cfd17a77ee63b9c907c
yuya <2333340431c@kindai.ac.jp>
2025-04-27 18:23:50 +0900

    First commit:kakeibo app

--- kakeibo_project/wsgi.py
+++ kakeibo_project/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for kakeibo_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-import django
+
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kakeibo_project.settings')
 
@@ -19,7 +9,14 @@ import django
 django.setup()
 from django.core.management import call_command
 call_command('migrate')
-# ===== ここまで追加 =====
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# ここでユーザーがまだいなければ管理者ユーザー作成
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+# ===== ここまで追加 =====
 
 application = get_wsgi_application()
